853d73b642eb58d1dcd0b21791e61b9c9b72ae3f
Erik Branco Queiroz <erikbrancoq@gmail.com>
2026-01-26 16:52:26 -0300

    feat: criação da classe Multa

---
+++ Multa.js
@@ -0,0 +1,50 @@
+// Arquivo: Multa.js
+
+// Definição da classe Multa. O uso de export indica que esta classe pode ser importada em outros arquivos.
+export class Multa {
+
+    // Atributos privados da classe Multa.
+    #id_multa;
+    #id_cliente;
+    #tipo_infracao;
+    #valor;
+    #data;
+    #status;
+
+    // Construtor da classe multa. 
+    constructor(id_cliente, tipo_infracao, valor, data, status) {
+        this.#id_cliente = id_cliente;
+        this.#tipo_infracao = tipo_infracao;
+        this.#valor = valor;
+        this.#data = data;
+        this.#status = status;
+
+        // Gera um ID único composto de 8 números para a multa.
+        this.#id_multa = Math.floor(Math.random() * 100000000).toString().padStart(8, '0');
+    }
+
+    // Getters para acessar os atributos privados da classe Multa.
+    get id_multa() {
+        return this.#id_multa;
+    }
+
+    get id_cliente() {
+        return this.#id_cliente;
+    }
+
+    get tipo_infracao() {
+        return this.#tipo_infracao;
+    }
+
+    get valor() {
+        return this.#valor;
+    }
+
+    get data() {
+        return this.#data;
+    }
+
+    get status() {
+        return this.#status;
+    }   
+}
