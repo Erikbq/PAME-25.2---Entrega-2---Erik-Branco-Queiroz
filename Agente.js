1a5cb184f7e3f4ab12b0f824b2f52682e676b1c3
Erik Branco Queiroz <erikbrancoq@gmail.com>
2026-01-26 16:40:07 -0300

    feat: criação da classe Agente

---
+++ Agente.js
@@ -0,0 +1,67 @@
+// Arquivo: Agente.js
+
+// Importa o módulo nativo "crypto" do Node.js para utilizar o hash.
+import crypto from 'node:crypto';
+
+// Definição da classe Agente.
+export class Agente {
+  // Atributos privados da classe Agente.
+  #id_agente;
+  #nome;
+  #cpf;
+  #email;
+  #senha_hash;
+  #numero_matricula;
+
+  // Construtor da classe Agente.
+  constructor(nome, cpf, email, senha_plana) {
+    this.#nome = nome;
+    this.#cpf = cpf;
+    this.#email = email;
+
+    // Gera um ID único composto de 8 números para o agente.
+    this.#id_agente = Math.floor(Math.random() * 100000000)
+      .toString()
+      .padStart(8, "0");
+
+    // Gera o hash da senha fornecida. Isso evita que a senha bruta seja armazenada diretamente.
+    this.#senha_hash = this.#gerarHashSenha(senha_plana);
+
+    // Gera um número de matrícula único com 6 números.
+    this.#numero_matricula = Math.floor(Math.random() * 1000000)
+      .toString()
+      .padStart(6, "0");
+  }
+
+  // Método privado para gerar o hash da senha utilizando o padrão SHA-256.
+  #gerarHashSenha(senha_analisada) {
+    return crypto.createHash("sha256").update(senha_analisada).digest("hex");
+  }
+
+  // Método público para validar a senha fornecida comparando o hash. Única ponte com o atributo senha_hash.
+  validarSenha(senha_tentativa) {
+    const hash_tentativa = this.#gerarHashSenha(senha_tentativa);
+    return hash_tentativa === this.#senha_hash;
+  }
+
+  // Getters para acessar os atributos privados da classe Agente.
+  get id_agente() {
+    return this.#id_agente;
+  }
+
+  get nome() {
+    return this.#nome;
+  }
+
+  get cpf() {
+    return this.#cpf;
+  }
+
+  get email() {
+    return this.#email;
+  }
+
+  get numero_matricula() {
+    return this.#numero_matricula;
+  }
+}
