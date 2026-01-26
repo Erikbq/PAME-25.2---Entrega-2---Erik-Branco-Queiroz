237596cfc5bd440dd8630fb2947f22bb561fd3c6
Erik Branco Queiroz <erikbrancoq@gmail.com>
2026-01-26 13:53:11 -0300

    feat: criação da classe Veiculo

---
+++ Veiculo.js
@@ -0,0 +1,32 @@
+export class Veiculo {
+
+    #placa;
+    #modelo;
+    #marca;
+    #cor;
+
+    constructor(placa, modelo, marca, cor) {
+
+        this.placa = placa;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.cor = cor;
+        
+    }
+
+    get placa() {
+        return this.#placa;
+    }
+
+    get modelo() {
+        return this.#modelo;
+    }
+
+    get marca() {
+        return this.#marca;
+    }   
+
+    get cor() { 
+        return this.#cor;
+    }
+}
