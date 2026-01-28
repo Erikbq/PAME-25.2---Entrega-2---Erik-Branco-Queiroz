bd8e2cda2c53c030c244e468abd5293e87826d7f
Erik Branco Queiroz <erikbrancoq@gmail.com>
2026-01-28 02:06:16 -0300

    feat: implementação de login e cadastro no Menu

--- Menu.js
+++ Menu.js
@@ -3,6 +3,14 @@
 // Importação da classe de contrele "Sistema"
 import { Sistema } from "./Sistema.js";
 
+// Importação das validações do aquivo de utilidades "utils"
+import { solicitar_email } from "./Solicitacoes.js";
+import { solicitar_senha } from "./Solicitacoes.js";
+import { solicitar_cpf } from "./Solicitacoes.js";
+import { solicitar_data } from "./Solicitacoes.js";
+import { solicitar_nome } from "./Solicitacoes.js";
+
+
 // Importação da biblioteca de input
 import readlineSync from 'readline-sync';
 
@@ -12,6 +20,7 @@ function main(){
     const sistema = new Sistema();
         
     let sair = false;
+    let tipo_usuario = 0;
 
     do{
         console.log("----------------------------------- Menu de Cadastro -----------------------------------\n",);
@@ -27,8 +36,94 @@ function main(){
         switch (opcao_cadastro) {
             case 1:
                 console.log("----------  Opção escolhida: Fazer Login  ----------\n");
-                const email = readlineSync.question("Digite o seu email: ");
-        } 
+
+                // Solicitação e verificação do email
+                const email = solicitar_email();
+
+                // Solicitação e verificação da senha
+                const senha = solicitar_senha();
+
+                const retorno = sistema.fazer_login(email, senha);
+                
+                if(retorno === true){
+                    console.log("Login realizado com sucesso!\n");
+                }
+                else{
+                    console.log("Email ou senha incorretos ou não cadastrados.\n");
+                    console.log("Se não tiver uma conta cadastrada escolha a opção 2 para se cadastrar\n");
+                }
+                break;
+
+            case 2:
+                console.log("----------  Opção escolhida: Fazer Cadastro  ----------\n");
+                console.log("Você está se cadastrando como?\n");
+                console.log("1- Condutor\n");
+                console.log("2- Agente de Trânsito\n");
+                console.log("Digite o número referente a opção escolhida\n");
+
+                const tipo_cadastro = readlineSync.questionInt();
+
+                switch (tipo_cadastro) {
+                    case 1:
+                        console.log("----------  Opção escolhida: Cadastro de Condutor  ----------\n");
+
+                        // Solicitar e verificar o nome
+                        const nome_condutor = solicitar_nome();
+
+                        // Solicita e verifica o cpf
+                        const cpf_condutor = solicitar_cpf();
+                    
+                        // Solicita e verifica a data
+                        const data_nascimento_condutor = solicitar_data();
+
+                        // Solicitação e verificação do email
+                        const email_condutor = solicitar_email();
+
+                        // Solicitação e verificação da senha
+                        const senha_condutor = solicitar_senha();
+
+                        const retorno_condutor = sistema.cadastro_condutor(nome_condutor, cpf_condutor, data_nascimento_condutor, email_condutor, senha_condutor);
+
+                        if(retorno_condutor === true){
+                            console.log("Login realizado com sucesso!\n");
+                        }
+                        else{
+                            console.log("Email ou senha incorretos ou não cadastrados.\n");
+                            console.log("Se não tiver uma conta cadastrada escolha a opção 2 para se cadastrar\n");
+                        }
+                        break;
+        
+                    case 2:
+                        console.log("----------  Opção escolhida: Cadastro de Agente  ----------\n");
+
+                        // Solicitar e verificar o nome
+                        const nome_agente = solicitar_nome();
+
+                        // Solicita e verifica o cpf
+                        const cpf_agente = solicitar_cpf();
+
+                        // Solicitação e verificação do email
+                        const email_agente = solicitar_email();
+
+                        // Solicitação e verificação da senha
+                        const senha_agente = solicitar_senha();
+
+                        const retorno_agente = sistema.cadastro_agente(nome_agente, cpf_agente, email_agente, senha_agente);
+
+                        if(retorno_agente === true){
+                            console.log("Login realizado com sucesso!\n");
+                        }
+                        else{
+                            console.log("Email ou senha incorretos ou não cadastrados.\n");
+                            console.log("Se não tiver uma conta cadastrada escolha a opção 2 para se cadastrar\n");
+                        }
+                        break;
+
+                    default:
+                        console.log("Opção inválida. Tente novamente\n");
+                        break;
+                }
+            }
     } while(!sair);
 }
 
